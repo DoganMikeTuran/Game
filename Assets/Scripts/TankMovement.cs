c3e82edcca1d1a6131704b6ab7a5ca067237eadf
Dogan Mike Turan <doganmiketuran@live.dk>
2019-08-20 11:55:47 +0200

    sds

---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    void Start(){
+     print("Hello world");
+}
+  
+
+    public void OnButtonClicked() {
+        GameObject.Find("Cube2").GetComponent<Rigidbody>().AddForce(0, 200, 0);        
+    }
+
+
+    public void HelloWorld(){
+    print("Hello world from button");
+    
+    }
+
+}
